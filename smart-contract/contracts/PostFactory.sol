5047216365faec14297c57774f5f93d85832643e
chuanshaof <chuanshaof@gmail.com>
2023-12-01 18:45:04 +0800

    added postfactory

---
+++ smart-contract/contracts/PostFactory.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.9;
+
+// Local imports
+import {Post} from "./Post.sol";
+import {PostType} from "./enums/PostType.sol";
+
+/**
+ * @title Post Contract Instance Factory
+ * @dev Facilitates the creation of new Post contract instances.
+ */
+library PostFactory {
+    /**
+     * @notice Creates a new MAIN type post
+     * @dev Deploys a Post contract with MAIN post type.
+     * @param _ownerAddress Address of the contract owning the new post
+     * @param _authorAddress Address of the user creating the post
+     * @param _content Post content in LSP2 JSONURL format
+     * @return address The address of the newly created Post contract
+     */
+    function createPost(
+        address _ownerAddress,
+        address _authorAddress,
+        bytes calldata _content
+    ) public returns (address) {
+        return
+            address(
+                new Post(
+                    _ownerAddress,
+                    _authorAddress,
+                    PostType.MAIN,
+                    _content,
+                    address(0)
+                )
+            );
+    }
+
+    /**
+     * @notice Creates a new COMMENT type post
+     * @dev Deploys a Post contract with COMMENT post type, referencing an existing post.
+     * @param _ownerAddress Address of the contract owning the new comment
+     * @param _authorAddress Address of the user creating the comment
+     * @param _content Comment content in LSP2 JSONURL format
+     * @param _referencedPostAddress Address of the post being commented on
+     * @return address The address of the newly created Post contract
+     */
+    function createCommentPost(
+        address _ownerAddress,
+        address _authorAddress,
+        bytes calldata _content,
+        address _referencedPostAddress
+    ) public returns (address) {
+        return
+            address(
+                new Post(
+                    _ownerAddress,
+                    _authorAddress,
+                    PostType.COMMENT,
+                    _content,
+                    _referencedPostAddress
+                )
+            );
+    }
+}
